72c6b4394fc432d7cdbb291f173c98623435fd39
martinsmandina <martinsmandina@gmail.com>
2020-10-25 22:18:47 +0300

    add class category

--- app/models.py
+++ app/models.py
@@ -44,4 +44,23 @@ class Pitch(db.Model):
     def __repr__(self):
         return f'Title {self.title}'
 
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+
+    def save_category(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_categories(cls):
+        categories = Category.query.all()
+        return categories
+
+
+
+
 
