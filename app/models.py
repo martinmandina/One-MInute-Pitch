2e156997b9f6ee5f6b542c7005a26d5d05ed6441
martinsmandina <martinsmandina@gmail.com>
2020-10-25 18:03:41 +0300

    create pitch class

--- app/models.py
+++ app/models.py
@@ -6,5 +6,11 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
 
-    def __repr__(self):
+class Pitch(db.model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(255))
+
+def __repr__(self):
         return f'User {self.username}'
