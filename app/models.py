4aec02a49f6e1eb1cde6e5942389ae312f560a3c
martinsmandina <martinsmandina@gmail.com>
2020-10-25 21:37:22 +0300

    run tests to confirm password_hash can verify

--- app/models.py
+++ app/models.py
@@ -9,6 +9,26 @@ class User(db.Model):
     pass_secure = db.Column(db.String(255))
 
     pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+
+
+    # securing passwords
+    @property
+    def password(self):
+        raise AttributeError('You can not read the password Attribute')
+
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
    
     def __repr__(self):
         return f'User {self.username}'
