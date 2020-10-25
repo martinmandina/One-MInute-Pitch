8bbb1d06c6f9c45aa1a0ab846a2ee4fc91c506eb
martinsmandina <martinsmandina@gmail.com>
2020-10-25 15:27:31 +0300

    set up environment variables in start.sh

--- config.py
+++ config.py
@@ -7,13 +7,6 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
 
 class ProdConfig(Config):
-      """
-    Production configuration child class
-
-    Args:
-        Config: The parent configuration class with General
-        configuration settings
-    """
     pass
 
 class DevConfig(Config):
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+export SECRET_KEY= e934357d1dcf802ff6bcdfab4759cf98
 
+python3.6 manage.py server
 
 
