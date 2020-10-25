686cbda3e1151032125be1d544049f501e4d5dcc
martinsmandina <martinsmandina@gmail.com>
2020-10-25 18:10:40 +0300

    create rshipbetween user and pitch class

--- app/models.py
+++ app/models.py
@@ -6,11 +6,16 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
 
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+   
+
 class Pitch(db.model):
     __tablename__ = 'pitches'
 
     id = db.Column(db.Integer,primary_key = True)
     title = db.Column(db.String(255))
+    
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 
 def __repr__(self):
         return f'User {self.username}'
