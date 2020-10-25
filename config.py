e7c586bb3257844cdef14448495360fc50929143
martinsmandina <martinsmandina@gmail.com>
2020-10-25 17:36:54 +0300

    import SQLAlchemy and initialized

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     General configuration parent class
     """
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://martinmandina:alicewambui@localhost/pitch'
 
 class ProdConfig(Config):
     pass
