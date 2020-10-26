cc86cafb2d75739d0318e13f642d31b4ba018971
martinsmandina <martinsmandina@gmail.com>
2020-10-26 14:12:39 +0300

    create new User

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,4 +3,13 @@ from ..models import User
 from .forms import RegistrationForm
 from .. import db
 
-
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegiForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
